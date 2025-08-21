b604bb443b542c42b37ce1d7531767f686c3cabb
chandanpandeya1 <chandanpandeya1@gmail.com>
2025-08-21 11:01:27 +0530

    Added not found

--- server.js
+++ server.js
@@ -73,3 +73,7 @@ function handleError(err, req, res, next) {
   console.error("Error occurred:", err);
   res.status(500).send("Internal Server Error");
 }
+
+function notFound(req, res, next) {
+  res.status(404).send("Not Found");
+}
